73cbf76c38dfc3235f64f7ae0930243f6d6e126e
keisersoze <filippomaganza@gmail.com>
2018-10-18 15:56:38 +0200

    using now embedded tomcatof Springboot

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -3,15 +3,15 @@ package bootwildfly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
+    
 }
 
