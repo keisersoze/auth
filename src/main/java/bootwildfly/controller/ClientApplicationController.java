324eee02f0d4be87a1ca63b4049bfe147f8b91ea
keisersoze <filippomaganza@gmail.com>
2018-10-23 14:54:12 +0200

    added dev profile

---
+++ src/main/java/bootwildfly/controller/ClientApplicationController.java
@@ -0,0 +1,9 @@
+package bootwildfly.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@RestController
+public class ClientApplicationController {
+}
